1bf2ab7f1c3060f2fb9e4276a9bfe61eb96e14b0
Pedro Orellana <pedrorellana21@gmail.com>
2025-12-12 11:49:14 -0800

    create User model and reference an idea with a user

---
+++ models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Hash passwords before saving
+UserSchema.pre("save", async function (next) {
+  //checking if the password was written. If it was not, just return the next function
+  if (!this.isModified("password")) return next();
+
+  //creating salt and hashing the password
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
